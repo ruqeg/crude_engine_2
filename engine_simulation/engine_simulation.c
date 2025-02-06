9261bfc82f2cf5e04b46a27026aa926caf129844
Yaroslav Yaroshenko <buwnertys@protonmail.com>
2025-02-06 16:24:34 +0100

    fix launcher

---
+++ engine_simulation/engine_simulation.c
@@ -0,0 +1,13 @@
+#include <engine.h>
+#include <cr.h>
+#include <assert.h>
+
+CR_EXPORT int cr_main( struct cr_plugin *ctx, enum cr_op operation )
+{
+  assert(ctx);
+  if (operation == CR_STEP)
+  {
+    crude_engine_update( ( crude_engine* )ctx->userdata );
+  }
+  return 0;
+}
--- launcher/launcher.cc
+++ launcher/launcher.cc
@@ -2,23 +2,48 @@
 #include <cr.h>
 #include <Windows.h>
 #include <engine.h>
+#include <filesystem>
+
+std::filesystem::path get_executable_path()
+{
+#ifdef _WIN32
+  wchar_t path[ MAX_PATH ] = { 0 };
+  GetModuleFileNameW( NULL, path, MAX_PATH );
+  return std::filesystem::path( path ).remove_filename();
+#else
+  return std::filesystem::canonical("/proc/self/exe");
+#endif
+}
+
+#define PLUGIN_PATH_STR( name ) get_executable_path().concat( CR_PLUGIN( name ) ).string()
 
 int APIENTRY wWinMain(_In_ HINSTANCE      hInstance,
                       _In_opt_ HINSTANCE  hPrevInstance,
                       _In_ LPWSTR         lpCmdLine,
                       _In_ int            nCmdShow)
 {
-  struct cr_plugin editor;
+  cr_plugin crude_editor, crude_engine_simulation;
   
   crude_engine engine = crude_engine_initialize( 1u );
 
-  cr_plugin_open( editor, CR_PLUGIN( "crude_editor" ) );
-  
+  crude_editor.userdata            = engine.world;
+  crude_engine_simulation.userdata = &engine;
+  std::filesystem::path a;
+  std::filesystem::path b;
+  //cr_plugin_open( crude_editor, CR_PLUGIN( "crude_editor" ) );
+  cr_plugin_open( crude_engine_simulation, PLUGIN_PATH_STR( "crude_engine_simulation" ).c_str() );
+
   while ( engine.running )
   {
+    //if ( cr_plugin_update( crude_editor ) == 0 )
+    //{
+      cr_plugin_update( crude_engine_simulation );
+    //}
     cr_plat_init();
   }
 
-  cr_plugin_close(editor);
+  //cr_plugin_close( crude_editor );
+  cr_plugin_close( crude_engine_simulation );
+
   return EXIT_SUCCESS;
 }
