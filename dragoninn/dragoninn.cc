e316f85509f445d00e0bd5784a1a552cdd56a274
Yaroslav Yaroshenko <buwnertys@protonmail.com>
2025-07-24 20:11:33 +0200

    tmp

--- dragoninn/dragoninn.cc
+++ dragoninn/dragoninn.cc
@@ -84,6 +84,24 @@ crude_dragoninn_initialize
   ax::NodeEditor::Config config;
   config.SettingsFile = "Simple.json";
   dragoninn->node_editor_context = ax::NodeEditor::CreateEditor( &config );
+  
+  crude_heap_allocator_initialize( &dragoninn->common_allocator, CRUDE_RMEGA( 32 ), "common_allocator" );
+  CRUDE_ARRAY_INITIALIZE_WITH_CAPACITY( dragoninn->render_graph_nodes, 0u, crude_heap_allocator_pack( &dragoninn->common_allocator ) );
+
+  crude_render_graph_resource resource;
+  resource.type = CRUDE_GFX_RENDER_GRAPH_RESOURCE_TYPE_ATTACHMENT;
+  resource.external = false;
+  crude_string_copy( resource.name, "resource 1", CRUDE_COUNTOF( resource.name ) );
+
+  crude_render_graph_node render_graph_node;
+  CRUDE_ARRAY_INITIALIZE_WITH_CAPACITY( render_graph_node.inputs, 0u, crude_heap_allocator_pack( &dragoninn->common_allocator ) );
+  CRUDE_ARRAY_INITIALIZE_WITH_CAPACITY( render_graph_node.outputs, 0u, crude_heap_allocator_pack( &dragoninn->common_allocator ) );
+  CRUDE_ARRAY_PUSH( render_graph_node.inputs, resource );
+  CRUDE_ARRAY_PUSH( render_graph_node.outputs, resource );
+  render_graph_node.enabled = true;
+  crude_string_copy( render_graph_node.name, "pass 1", CRUDE_COUNTOF( resource.name ) );
+  render_graph_node.type = CRUDE_GFX_RENDER_GRAPH_NODE_TYPE_GRAPHICS;
+  CRUDE_ARRAY_PUSH( dragoninn->render_graph_nodes, render_graph_node );
 
   CRUDE_ECS_SYSTEM_DEFINE( dragoninn->engine->world, dragoninn_update_system_, EcsOnUpdate, dragoninn, {
     { .id = ecs_id( crude_input ) },
@@ -120,6 +138,21 @@ crude_dragoninn_update
   }
 }
 
+static const char*
+to_type
+(
+  crude_gfx_render_graph_resource_type type
+)
+{
+  switch ( type )
+  {
+  case CRUDE_GFX_RENDER_GRAPH_RESOURCE_TYPE_ATTACHMENT: return "attachment";
+  case CRUDE_GFX_RENDER_GRAPH_RESOURCE_TYPE_BUFFER: return "buffer";
+  case CRUDE_GFX_RENDER_GRAPH_RESOURCE_TYPE_TEXTURE: return "texture";
+  }
+  return "unknown";
+}
+
 void
 dragoninn_update_system_
 (
@@ -140,29 +173,141 @@ dragoninn_update_system_
   ImGui_ImplSDL3_NewFrame();
   ImGui::NewFrame();
 
-  ImGui::DockSpaceOverViewport(0u, ImGui::GetMainViewport());
-  {
-    ImGui::Begin( "main", NULL );
-    ImGui::End();
-  }
-
-  
   auto& io = ImGui::GetIO();
   
   axn::SetCurrentEditor( dragoninn->node_editor_context );
+  
   axn::Begin("My Editor", ImVec2(0.0, 0.0f));
+  
   int uniqueId = 1;
-  // Start drawing nodes.
-  axn::BeginNode(uniqueId++);
-      ImGui::Text("Node A");
-      axn::BeginPin(uniqueId++, axn::PinKind::Input);
-          ImGui::Text("-> In");
-      axn::EndPin();
+  for ( uint32 i = 0; i < CRUDE_ARRAY_LENGTH( dragoninn->render_graph_nodes ); ++i )
+  {
+    crude_render_graph_node *graph_node = &dragoninn->render_graph_nodes[ i ];
+    axn::BeginNode(uniqueId++);
+    
+    ImGui::PushItemWidth( 300 );
+    ImGui::InputText( "name", graph_node->name, CRUDE_COUNTOF( graph_node->name ) );
+    ImGui::PopItemWidth();
+    ImGui::Checkbox( "enabled", &graph_node->enabled ); ImGui::SameLine();
+    int type = graph_node->type;
+    ImGui::RadioButton( "graphics", &type, 0 ); ImGui::SameLine();
+    ImGui::RadioButton( "compute", &type, 1 );
+    graph_node->type = CRUDE_CAST( crude_gfx_render_graph_node_type, type );
+    
+    ImGui::Separator( );
+
+    for ( uint32 i = 0; i < crude_max( CRUDE_ARRAY_LENGTH( graph_node->inputs ), CRUDE_ARRAY_LENGTH( graph_node->outputs ) ); ++i )
+    {
+      if ( i < CRUDE_ARRAY_LENGTH( graph_node->inputs ) )
+      {
+        axn::BeginPin( uniqueId++, axn::PinKind::Input );
+
+        ImGui::PushID( uniqueId++ );
+        ImGui::Text( "-> [in]" );
+        ImGui::PushItemWidth( 300 );
+        ImGui::Text( "name %s", graph_node->inputs[ i ].name );
+        ImGui::PopItemWidth();
+        ImGui::Text( graph_node->inputs[ i ].external ? "external" : "not external" ); ImGui::SameLine();
+        switch ( graph_node->inputs[ i ].type )
+        {
+        case CRUDE_GFX_RENDER_GRAPH_RESOURCE_TYPE_ATTACHMENT:
+        {
+          ImGui::Text( "attachment" );
+          break;
+        }
+        case CRUDE_GFX_RENDER_GRAPH_RESOURCE_TYPE_BUFFER:
+        {
+          ImGui::Text( "buffer" );
+          break;
+        }
+        case CRUDE_GFX_RENDER_GRAPH_RESOURCE_TYPE_TEXTURE:
+        {
+          ImGui::Text( "texture" );
+          break;
+        }
+        }
+
+        ImGui::PopID( );
+
+        axn::EndPin( );
+      }
       ImGui::SameLine();
-      axn::BeginPin(uniqueId++, axn::PinKind::Output);
-          ImGui::Text("Out ->");
-      axn::EndPin();
-  axn::EndNode();
+      ImGui::Dummy( ImVec2( 10, 0 ) ); /* Hacky magic number to space out the output pin. */
+      ImGui::SameLine();
+      if ( i < CRUDE_ARRAY_LENGTH( graph_node->outputs ) )
+      {
+        axn::BeginPin( uniqueId++, axn::PinKind::Output );
+
+        ImGui::PushID( uniqueId++ );
+
+        ImGui::Text( "[out] ->" );
+        ImGui::PushItemWidth( 300 );
+        ImGui::InputText( "name", graph_node->outputs[ i ].name, CRUDE_COUNTOF( graph_node->outputs[ i ].name ) );
+        ImGui::PopItemWidth();
+        ImGui::Checkbox( "external", &graph_node->outputs[ i ].external ); ImGui::SameLine();
+        type = graph_node->outputs[ i ].type;
+        ImGui::RadioButton( "buffer", &type, 0 ); ImGui::SameLine();
+        ImGui::RadioButton( "texture", &type, 1 ); ImGui::SameLine();
+        ImGui::RadioButton( "attachment", &type, 2 );
+        graph_node->outputs[ i ].type = CRUDE_CAST( crude_gfx_render_graph_resource_type, type );
+
+        ImGui::PopID( );
+
+        axn::EndPin();
+      }
+    }
+    
+    axn::EndNode();
+  }
+            // ==================================================================================================
+            // Link Drawing Section
+
+            for (auto& linkInfo : dragoninn->m_Links)
+                axn::Link(linkInfo.Id, linkInfo.InputId, linkInfo.OutputId);
+
+            // ==================================================================================================
+            // Interaction Handling Section
+            // This was coppied from BasicInteration.cpp. See that file for commented code.
+            static int                  m_NextLinkId = 100; 
+            // Handle creation action ---------------------------------------------------------------------------
+            if (axn::BeginCreate())
+            {
+                axn::PinId inputPinId, outputPinId;
+                if (axn::QueryNewLink(&inputPinId, &outputPinId))
+                {
+                    if (inputPinId && outputPinId)
+                    {
+                        if (axn::AcceptNewItem())
+                        {
+                            dragoninn->m_Links.push_back({ axn::LinkId(m_NextLinkId++), inputPinId, outputPinId });
+                            axn::Link(dragoninn->m_Links.back().Id, dragoninn->m_Links.back().InputId, dragoninn->m_Links.back().OutputId);
+                        }
+                    }
+                }
+            }
+            axn::EndCreate();
+
+            // Handle deletion action ---------------------------------------------------------------------------
+            if (axn::BeginDelete())
+            {
+                axn::LinkId deletedLinkId;
+                while (axn::QueryDeletedLink(&deletedLinkId))
+                {
+                    if (axn::AcceptDeletedItem())
+                    {
+                        for (auto& link : dragoninn->m_Links)
+                        {
+                            if (link.Id == deletedLinkId)
+                            {
+                                dragoninn->m_Links.erase(&link);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            axn::EndDelete();
+
   axn::End();
   axn::SetCurrentEditor(nullptr);
 
--- dragoninn/dragoninn.h
+++ dragoninn/dragoninn.h
@@ -12,6 +12,29 @@
 
 namespace axn = ax::NodeEditor;
 
+typedef struct LinkInfo
+{
+  axn::LinkId                                         Id;
+  axn::PinId                                          InputId;
+  axn::PinId                                          OutputId;
+} LinkInfo;
+
+typedef struct crude_render_graph_resource
+{
+  crude_gfx_render_graph_resource_type                     type;
+  bool                                                     external;
+  char                                                     name[ 512 ];
+} crude_render_graph_resource;
+
+typedef struct crude_render_graph_node
+{
+  crude_render_graph_resource                             *inputs;
+  crude_render_graph_resource                             *outputs;
+  bool                                                     enabled;
+  char                                                     name[ 512 ];
+  crude_gfx_render_graph_node_type                         type;
+} crude_render_graph_node;
+
 typedef struct crude_dragoninn
 {
   bool                                                     working;
@@ -20,6 +43,9 @@ typedef struct crude_dragoninn
   ImGuiContext                                            *imgui_context;
   SDL_Renderer                                            *sdl_renderer;
   axn::EditorContext                                      *node_editor_context;
+  crude_heap_allocator                                     common_allocator;
+  crude_render_graph_node                                 *render_graph_nodes;
+  ImVector<LinkInfo>   m_Links;
 } crude_dragoninn;
 
 CRUDE_API void
